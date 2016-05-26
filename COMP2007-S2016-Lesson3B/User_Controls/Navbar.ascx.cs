dcd691f40caa259f6cb1415ba5e1a20bf1456dcb
kingnot <kingnot0524@gmail.com>
2016-05-26 14:11:49 -0400

    added navbar user control

---
+++ COMP2007-S2016-Lesson3B/User_Controls/Navbar.ascx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/**
+ * @author: Fei Wang
+ * @date: May 26. 2016
+ * @version: 0.0.1
+ */
+
+namespace COMP2007_S2016_Lesson3B
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+
+        /* *
+         * THis method adds a css class of "active" to list items
+         * related to navigation links of each page
+         * 
+         * @method SetActivePage
+         * @return {void}
+         */
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
